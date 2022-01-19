ca5b8de2a53aaefe79709a137c19d0abafdfb1cb
reedsutliff <sutliff.reed@gmail.com>
2022-01-19 14:40:33 -0500

    modified VIN validation implementation to verify VIN againstt NHTSA, added tests

--- Validation/VIN.cs
+++ Validation/VIN.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Polly;
+using System;
 using System.Activities;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
+using System.Web;
 
 namespace Impower.DocumentUnderstanding.Validation
 {
@@ -14,33 +21,56 @@ namespace Impower.DocumentUnderstanding.Validation
 
     internal static class ValidationExtensions
     {
-        //from https://gist.github.com/deeja/008c2c36764f11cbc02818e7d793738a
-        internal static int Transliterate(char c)
+        public static readonly string[] VinQueryFilter = {
+                "Error Code",
+                "Error Text",
+                "Make",
+                "Manufacturer Name",
+                "Model",
+                "Model Year",
+                "Trim",
+                "Vehicle Type",
+                "Doors",
+                "Body Class"
+        };
+        internal static JObject GetVinInfoFromNHTSA(string vin)
         {
-            return "0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ".IndexOf(c) % 10;
+            HttpWebResponse response = null;
+            var url = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/" + HttpUtility.UrlEncode(vin) + "?format=json";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Accept = "*/*";
+            var retryPolicy = Policy.Handle<Exception>().WaitAndRetry(
+                retryCount: 3, sleepDurationProvider: _ => TimeSpan.FromSeconds(3)
+            );
+            retryPolicy.Execute(() =>
+            {
+                Console.WriteLine($"Validating VIN ({vin}) against NHTSA database...");
+                response = (HttpWebResponse)httpWebRequest.GetResponse();
+            });
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                return JObject.Parse(streamReader.ReadToEnd());
+            }
         }
 
-        //from https://gist.github.com/deeja/008c2c36764f11cbc02818e7d793738a
-        internal static char GetCheckDigit(string vin)
+        public static bool ValidateVinWithNHTSA(string vin, out Dictionary<string, string> properties)
         {
-            string map = "0123456789X";
-            string weights = "8765432X098765432";
-            int sum = 0;
-            for (int i = 0; i < 17; ++i)
-            {
-                sum += Transliterate(vin[i]) * map.IndexOf(weights[i]);
+            if(vin.Length != 17) {
+                properties = null;
+                return false;
             }
-            return map[sum % 11];
-        }
-
-        //from https://gist.github.com/deeja/008c2c36764f11cbc02818e7d793738a
-        internal static bool Validate(string vin)
-        {
-            if (vin.Length != 17)
-            {
-                throw new InvalidVinException("VIN was not 17 characters long.");
-            }
-            return GetCheckDigit(vin) == vin[8];
+            JObject response = GetVinInfoFromNHTSA(vin);
+            JArray values = response["Results"] as JArray;
+            properties = values.Select(
+                jToken => jToken as JObject
+            ).Where(
+                jObject => VinQueryFilter.Contains(jObject["Variable"].ToString())
+            ).ToDictionary(
+                jObject => jObject["Variable"].ToString(),
+                jObject => jObject["Value"].ToString()
+            );
+            return properties["Error Code"].ToString() == "0";
         }
     }
 
@@ -49,31 +79,40 @@ namespace Impower.DocumentUnderstanding.Validation
     {
         private readonly Regex vinRegex = new Regex(@"[^A-HJ-NPR-Za-hj-npr-z\d]");
 
+        [Category("Input")]
         [DisplayName("VIN Number")]
         [RequiredArgument]
         public InOutArgument<string> VIN { get; set; }
 
+        [Category("Input")]
         [DisplayName("Attempt To Fix OCR?")]
         [Description("Replace I's, O's, and Q's to attempt to fix incorrect OCR.")]
         [DefaultValue(true)]
         public InArgument<bool> FixOCR { get; set; }
 
+        [Category("Output")]
+        [DisplayName("VIN Properties")]
+        [Description("List of properties associated with VIN, per NHTSA")]
+        public OutArgument<Dictionary<string,string>> Properties { get; set; }
+
+        [Category("Output")]
+        [DisplayName("Valid VIN?")]
+        [Description("Indicates if the VIN was validated.")]
+        public OutArgument<bool> Valid { get; set; }
         protected override void Execute(CodeActivityContext context)
         {
+            Dictionary<string, string> properties;
             string sanitizedVin = VIN.Get(context).ToUpper();
+            Console.WriteLine($"Sanitized VIN: '{sanitizedVin}'");
             if (FixOCR.Get(context))
             {
                 sanitizedVin = sanitizedVin.Replace("I", "1").Replace("O", "0").Replace("Q", "9");
             }
             sanitizedVin = vinRegex.Replace(sanitizedVin, String.Empty);
-            if (ValidationExtensions.Validate(sanitizedVin))
-            {
-                VIN.Set(context, sanitizedVin);
-            }
-            else
-            {
-                throw new InvalidVinException("Check digit did not match.");
-            }
+            bool valid = ValidationExtensions.ValidateVinWithNHTSA(sanitizedVin, out properties);
+            VIN.Set(context, sanitizedVin);
+            Properties.Set(context, properties);
+            Valid.Set(context, valid);
         }
     }
 }
